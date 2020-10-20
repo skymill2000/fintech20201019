1c6223f238b200f953714091de2107e6dc277dbe
gwanwoo-you <skymill2000@gmail.com>
2020-10-20 15:22:20 +0900

    뉴스 데이터 파싱 완료

--- request.js
+++ request.js
@@ -4,7 +4,14 @@ request(
   function (error, response, body) {
     var parsedData = JSON.parse(body);
     //JSON String 으로 되어있는 데이터를 Js Object 변환
-    console.log(parsedData.totalResults);
     //#work2 Title 목록만 조회하기 (for, object select)
+    for (let index = 0; index < parsedData.articles.length; index++) {
+      const element = parsedData.articles[index];
+      console.log(index + 1, element.title);
+    }
   }
+);
+request(
+  "http://newsapi.org/v2/top-headlines?apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5&country=kr&category=business",
+  function (error, response, body) {}
 );
