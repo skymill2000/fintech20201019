cbb3cd7260bc8b2b1555178a6e41f27e69d7a1ef
gwanwoo-you <skymill2000@gmail.com>
2020-10-20 15:07:23 +0900

    실습 2 데이터 파싱

--- request.js
+++ request.js
@@ -1,4 +1,10 @@
 const request = require("request");
-request("https://www.naver.com", function (error, response, body) {
-  console.log("body:", body); // Print the HTML for the Google homepage.
-});
+request(
+  "http://newsapi.org/v2/top-headlines?apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5&country=kr&category=business",
+  function (error, response, body) {
+    var parsedData = JSON.parse(body);
+    //JSON String 으로 되어있는 데이터를 Js Object 변환
+    console.log(parsedData.totalResults);
+    //#work2 Title 목록만 조회하기 (for, object select)
+  }
+);
