73ecf4643dc301a7c8f33512acd342691b6106cb
gwanwoo-you <skymill2000@gmail.com>
2020-10-20 10:32:40 +0900

    실습 1 : 사칙연산 기능 만들기

---
+++ sample/datatype.js
@@ -0,0 +1,22 @@
+var length = 16; // Number
+var lastName = "Johnson"; // String
+
+//# in java
+// private int length = 16;
+// private String lastName = "Johnson";
+
+var x = { firstName: "John", lastName: "Doe" }; // Object
+// var x = 16 + "Volvo";
+console.log(x);
+
+var y = "16" + "Volvo";
+console.log(y);
+
+var z = 16 + "Volvo";
+console.log(z);
+
+var a = "Volvo" + 16;
+console.log(a);
+
+var b = 16 + 4 + "Volvo";
+console.log(b);
