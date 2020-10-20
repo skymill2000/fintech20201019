618cff231dea0979d42f58d00a83000e8d2a8441
gwanwoo-you <skymill2000@gmail.com>
2020-10-20 16:12:00 +0900

    mysql 연동 실습 완료 (사용자 조회)

--- dbConn.js
+++ dbConn.js
@@ -8,9 +8,9 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+connection.query("SELECT * FROM user;", function (error, results, fields) {
   if (error) throw error;
-  console.log("The solution is: ", results[0].solution);
+  console.log(results);
 });
 
 connection.end();
