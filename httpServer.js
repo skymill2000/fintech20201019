b15a46c281ae7c4fdbd36d07ee9ff1350a2be104
gwanwoo-you <skymill2000@gmail.com>
2020-10-20 14:29:09 +0900

    html 코드로 response 만들기

--- httpServer.js
+++ httpServer.js
@@ -4,7 +4,7 @@ http
   .createServer(function (req, res) {
     var body = "hello Server";
     console.log("요청 발생");
-    res.setHeader("Content-Type", "text/plain; charset=utf-8");
-    res.end("안녕하세요");
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end("<html><h1>test</h1><input type='text'/></html>");
   })
-  .listen(3001);
+  .listen(3000);
