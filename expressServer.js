463c2c5d84eeb37e165fca99cd5745dcc05373e3
gwanwoo-you <skymill2000@gmail.com>
2020-10-21 11:36:49 +0900

    json 데이터 허용 urlencoded 데이터 허용 코드 추가

--- expressServer.js
+++ expressServer.js
@@ -6,12 +6,23 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 //여러가지 뷰 엔진중에서 ejs 를 사용하겠다 
 
+app.use(express.json());
+//JSON 형태의 데이터 전송을 허용하겠다
+app.use(express.urlencoded({ extended: false }));
+//urlencoded 형식의 데이터 전송을 허용하겠다
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
 app.get('/hello', function (req, res) {
   res.render('test');
+})
+
+app.post('/getData',function(req, res){
+  console.log(req.body);
+  // var getUserId = req.body.sendUserId;
+  // var getUserPassword = req.body.sendUserPassword;
 })
 
 
