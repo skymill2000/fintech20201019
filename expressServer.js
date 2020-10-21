b1ede7cd8c359589dd06f7c7dcd973808faea8ac
gwanwoo-you <skymill2000@gmail.com>
2020-10-21 10:42:22 +0900

    ejs 추가

---
+++ expressServer.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.set('views', __dirname + '/views');
+//뷰 파일이 있는 디렉토리를 지정합니다.
+app.set('view engine', 'ejs');
+//여러가지 뷰 엔진중에서 ejs 를 사용하겠다 
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/hello', function (req, res) {
+  res.send('Hello New hello')
+})
+
+app.get('/user', function(req, res){
+  res.send('good!');
+})
+
+
+app.listen(3000)
