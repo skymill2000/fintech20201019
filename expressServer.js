8beeaa271b431c0cc601a067322d671f11f8fbac
gwanwoo-you <skymill2000@gmail.com>
2020-10-23 14:33:44 +0900

    출금이체 기능 서버 완성

--- expressServer.js
+++ expressServer.js
@@ -252,7 +252,49 @@ app.post('/transactionlist', auth, function(req, res){
 })
 
 app.post('/withdraw', auth, function(req, res){
-  //#출금 기능만 완성해 주세요 !
+  var userId = req.decoded.userId;
+
+  var finusenum = req.body.fin_use_num; 
+  var to_fin_use_num = req.body.to_fin_use_num; 
+  var amount = req.body.amount; 
+
+  var userSearchSql = "SELECT * FROM user WHERE id = ?";
+  var countnum = Math.floor(Math.random() * 1000000000) + 1;
+  var transId = "T991599190U" + countnum; //이용기관번호 본인것 입력
+  connection.query(userSearchSql,[userId], function(err, results){
+    if(err) throw err;
+    else {
+      var option = {
+        method: "POST",
+        url: "https://testapi.openbanking.or.kr/v2.0/transfer/withdraw/fin_num",
+        headers: {
+          "Authorization" : "Bearer " + results[0].accesstoken
+        },
+        //form 형태는 form / 쿼리스트링 형태는 qs / json 형태는 json ***
+        json: {
+          "bank_tran_id": transId, 
+          "cntr_account_type": "N",
+          "cntr_account_num": "7832932596", 
+          "dps_print_content": "쇼핑몰환불", 
+          "fintech_use_num": finusenum, 
+          "wd_print_content": "오픈뱅킹출금",
+          "tran_amt": amount,
+          "tran_dtime": "20201023110700",
+          "req_client_name": "홍길동",
+          "req_client_num": "HONGGILDONG1234",
+          "req_client_fintech_use_num" : finusenum,
+          "transfer_purpose": "ST",
+          "recv_client_name": "진상언",
+          "recv_client_bank_code": "097",
+          "recv_client_account_num": "7832932596"
+        },
+      };
+      request(option, function(err, response, body){
+        var withdrawResult = body; //JSON 오브젝트를 JS 오브젝트로 변경
+        console.log(withdrawResult);
+      })    
+    }
+  })
 })
 
 
